44c9980af116063b06f8da99c613ce65cb94184b
Luke Yu <yuhaonong@foxmail.com>
2018-05-21 00:36:56 +0800

    add test images and demo cmd

--- app/src/main/java/com/tencent/lukeyu/humanbodydetect/MainActivity.kt
+++ app/src/main/java/com/tencent/lukeyu/humanbodydetect/MainActivity.kt
@@ -1,11 +1,14 @@
 package com.tencent.lukeyu.humanbodydetect
 
-import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
-
+//    curl -X POST "https://api-cn.faceplusplus.com/humanbodypp/v1/detect" -F "api_key=<api_key>" \
+//    -F "api_secret=<api_secret>" \
+//    -F "image_file=@image_file.jpg" \
+//    -F "return_attributes=gender"
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
