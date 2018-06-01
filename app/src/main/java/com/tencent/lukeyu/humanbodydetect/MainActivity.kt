150e28fe9e77fd24e5c3dcda9474b7ce313102d5
Luke Yu <yuhaonong@foxmail.com>
2018-06-02 00:51:19 +0800

    use image_file api

--- app/src/main/java/com/tencent/lukeyu/humanbodydetect/MainActivity.kt
+++ app/src/main/java/com/tencent/lukeyu/humanbodydetect/MainActivity.kt
@@ -2,15 +2,19 @@ package com.tencent.lukeyu.humanbodydetect
 
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
+import android.util.Log
 import kotlinx.android.synthetic.main.activity_main.*
 import org.json.JSONObject
 import java.io.DataOutputStream
 import java.net.HttpURLConnection
 import java.net.URL
-import java.util.UUID
+import java.util.*
+import kotlin.collections.LinkedHashMap
+
 
 const val API_URL = "https://api-cn.faceplusplus.com/humanbodypp/v1/detect"
 const val IMAGE_URL = "http://img.chinaluxus.com/pic/view/2011/07/20/20110720085657323.jpg"
+
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -19,68 +23,81 @@ class MainActivity : AppCompatActivity() {
 
         val keyJson = assets.open("api.store")?.bufferedReader()?.use { it.readText() }
         val jsonObj = JSONObject(keyJson)
-        val api_key = jsonObj.get("api_key")
-        val api_secret = jsonObj.get("api_secret")
+        val apiKey = jsonObj.get("api_key") as String
+        val apiSecret = jsonObj.get("api_secret") as String
 
         Thread( {
-            val boundary = UUID.randomUUID().toString()
-            val url = URL(API_URL)
-            val conn = url.openConnection() as HttpURLConnection
-            conn.connectTimeout = 5000
-//            conn.setRequestProperty("Content-Type", "multipart/form-data;boundary=$boundary")
-            conn.setRequestProperty("Content-Type","application/x-www-form-urlencoded")
-            conn.setRequestProperty("Connection", "Keep-Alive")
-            conn.setRequestProperty("Charset", "UTF-8")
-            conn.requestMethod = "POST"
-            conn.doInput = true
-            conn.doOutput = true
-            conn.useCaches = false
-//            conn.connect()
-
             //    curl -X POST "https://api-cn.faceplusplus.com/humanbodypp/v1/detect"
             //    -F "api_key=<api_key>" \
             //    -F "api_secret=<api_secret>" \
             //    -F "image_file=@image_file.jpg" \
             //    -F "return_attributes=gender"
+
+            val url = URL(API_URL)
+            val conn = url.openConnection() as HttpURLConnection
+            conn.requestMethod = "POST"
+            conn.connectTimeout = Companion.TIME_OUT
+            conn.readTimeout = Companion.TIME_OUT
+            conn.doInput = true
+            conn.doOutput = true
+            conn.useCaches = false
+            conn.setRequestProperty("Connection", "Keep-Alive")
+            conn.setRequestProperty("Content-Type", "${Companion.CONTENT_TYPE};boundary=${Companion.BOUNDARY}")
+            conn.setRequestProperty("Charset", "UTF-8")
+
+            val strParams = LinkedHashMap<String, String>()
+            strParams["api_key"] = apiKey
+            strParams["api_secret"] = apiSecret
+            strParams["return_attributes"] = "gender"
             val outputStream = DataOutputStream(conn.outputStream)
-            outputStream.writeBytes("api_key=$api_key")
-            outputStream.writeBytes("&api_secret=$api_secret")
-            outputStream.writeBytes("&image_url=$IMAGE_URL")
-            outputStream.writeBytes("&return_attributes=gender")
+            val paramPart = getStrParams(strParams).toString()
+            outputStream.writeBytes(paramPart)
+            Log.i("DEBUG", paramPart)
+
+            //文件上传
+            val fileSb = StringBuilder()
+            fileSb.append(PREFIX).append(BOUNDARY).append(LINE_END)
+                    .append("""Content-Disposition: form-data; name="image_file"; filename="pony.jpg"$LINE_END""")
+                    .append("Content-Type: image/jpg$LINE_END")
+                    .append(LINE_END)// 参数头设置完以后需要两个换行，然后才是参数内容
+            outputStream.writeBytes(fileSb.toString())
+            assets.open("pony.jpg")?.buffered()?.use { outputStream.write(it.readBytes()) }
+            outputStream.writeBytes(LINE_END + PREFIX + BOUNDARY + PREFIX + LINE_END)
             outputStream.flush()
-            outputStream.close()
 
             val respCode = conn.responseCode
-            val msg: String?
-            if (respCode != 200) {
-                msg = conn.errorStream?.bufferedReader()?.use { it.readText() }
+            val msg = if (respCode != 200) {
+                conn.errorStream?.bufferedReader()?.use { it.readText() }
             } else {
-                msg = conn.inputStream?.bufferedReader()?.use { it.readText() }
+                conn.inputStream?.bufferedReader()?.use { it.readText() }
             }
             runOnUiThread { this@MainActivity.sample_text.text = respCode.toString() + msg}
-//            val inputStream = conn.inputStream
-//            val reader = BufferedReader(InputStreamReader(inputStream))
-//            val strBuilder = StringBuilder()
-//            reader.forEachLine { strBuilder.append(it) }
-//            reader.close()
             conn.disconnect()
-
-//            runOnUiThread { this@MainActivity.sample_text.text = strBuilder.toString() }
         }).start()
 
     }
 
     /**
-     * A native method that is implemented by the 'native-lib' native library,
-     * which is packaged with this application.
+     * 对post参数进行编码处理
      */
-    external fun stringFromJNI(): String
+    private fun getStrParams(strParams: Map<String, String>): StringBuilder {
+        val strSb = StringBuilder()
+        for ((key, value) in strParams) {
+            strSb.append(PREFIX).append(BOUNDARY).append(LINE_END)
+                    .append("""Content-Disposition: form-data; name="$key"$LINE_END""")
+                    .append(LINE_END)// 参数头设置完以后需要两个换行，然后才是参数内容
+                    .append(value)
+                    .append(LINE_END)
+        }
+        return strSb
+    }
 
     companion object {
-
-        // Used to load the 'native-lib' library on application startup.
-        init {
-            System.loadLibrary("native-lib")
-        }
+        private const val TIME_OUT = 8 * 1000                          //超时时间
+        private const val CHARSET = "UTF-8"                         //编码格式
+        private const val PREFIX = "--"                            //前缀
+        private val BOUNDARY = UUID.randomUUID().toString()  //边界标识 随机生成
+        private const val CONTENT_TYPE = "multipart/form-data"     //内容类型
+        private const val LINE_END = "\n"                        //换行
     }
 }
